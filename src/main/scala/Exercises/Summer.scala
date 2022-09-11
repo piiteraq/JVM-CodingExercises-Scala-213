e9a771e0de6020a141176cb6b78d264d63dcb6fc
Peter H Christensen <peter.christensen@att.net>
2022-09-11 08:29:02 -0700

    Added new exercise

---
+++ src/main/scala/Exercises/Summer.scala
@@ -0,0 +1,19 @@
+package Exercises
+
+import ChecksumAccumulator.calculate
+
+//object Summer {
+//  def main(args: Array[String]) = {
+//    for (arg <- args)
+//      println(arg + ": " + calculate(arg))
+//  }
+//}
+
+object Summer extends App {
+  for (season <- List("fall", "winter", "spring", "summer"))
+    println(season + ": " + calculate(season))
+
+  // Command line args still available ..
+  for (arg <- args)
+    println(arg)
+}
