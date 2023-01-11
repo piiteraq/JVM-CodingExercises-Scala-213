53b81410f522cff78080fb9a0156498485b8523c
Peter H Christensen <peter.christensen@att.net>
2023-01-11 07:12:33 -0800

    Add Reg usage Example

---
+++ src/main/scala/CatsEffect/RefTest.scala
@@ -0,0 +1,37 @@
+package CatsEffect
+
+import cats.effect.unsafe.implicits.global
+import cats.effect.{IO, IOApp, Ref, Sync}
+import cats.syntax.all._
+
+class Worker[F[_]](number: Int, ref: Ref[F, Int])(implicit F: Sync[F]) {
+
+  private def putStrLn(value: String): F[Unit] = F.delay(println(value))
+
+  def start: F[Unit] =
+    for {
+      c1 <- ref.get
+      _  <- putStrLn(show"#$number >> $c1")
+      c2 <- ref.modify(x => (x + 1, x))
+      _  <- putStrLn(show"#$number >> $c2")
+    } yield ()
+
+}
+
+object RefTest extends IOApp.Simple {
+
+  val program: IO[Unit] =
+    for {
+      ref <- Ref[IO].of(0)
+      w1  = new Worker[IO](1, ref)
+      w2  = new Worker[IO](2, ref)
+      w3  = new Worker[IO](3, ref)
+      _   <- List(
+        w1.start,
+        w2.start,
+        w3.start
+      ).parSequence.void
+    } yield ()
+
+  val run = program
+}
